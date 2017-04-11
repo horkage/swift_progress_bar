e23d59ebc83b8b137f70a09dfcf583178ab7665a
Michael Wood <mwood@Michaels-iMac.local>
2017-04-10 17:05:12 -0700

    initial commit

--- ProgressBar/ViewController.swift
+++ ProgressBar/ViewController.swift
@@ -9,6 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count: Float = 0.0
+    var min: Float = 0.0
+    var max: Float = 1.0
+    var int: Float = 0.1
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var theButton: UIButton!
+    
+    @IBAction func toggleProgressBar(_ sender: UIButton) {
+        if theButton.title(for: .normal) == "Start" || theButton.title(for: .normal) == "Resume" {
+            theButton.setTitle("Pause", for: .normal)
+            max = 1.0
+            startProgress()
+        } else {
+            theButton.setTitle("Resume", for: .normal)
+            stopProgress()
+        }
+    }
+    
+    func startProgress() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            while self.count <= self.max {
+                DispatchQueue.main.async {
+                    self.progressBar.setProgress(self.count, animated: true)
+                }
+                self.count += self.int
+                
+                if Int(self.count) == Int(self.max) {
+                    DispatchQueue.main.async {
+                        self.theButton.setTitle("Start", for: .normal)
+                        self.count = self.min
+                        self.progressBar.setProgress(self.min, animated: true)
+                    }
+                } else {
+                    sleep(1)
+                }
+            }
+            
+        }
+    }
+    
+    func stopProgress() {
+        max = -1.0
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
